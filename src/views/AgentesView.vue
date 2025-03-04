997a003e98bb8f5286b964146532e11d3157cb75
Fernando Diaz Taballione <fdtaballione@gmail.com>
2025-03-04 12:21:22 -0300

    CargaFamiliar02

--- src/views/AgentesView.vue
+++ src/views/AgentesView.vue
@@ -1,7 +1,30 @@
 <script setup>
 import PersonasList from '@/components/Agentes/PersonasList.vue'
+import CargaFamiliarList from '@/components/Agentes/CargaFamiliarList.vue'
+import { ref } from 'vue'
+
+const persona = {
+  ID: 337,
+  DNI: 12345678,
+  APELLIDOYNOMBRE: 'NOMBRE DE LA PERSONA'
+}
+
+const personaEdicion = ref(persona)
+const titulo = ref('Listado de hojas')
+
+function setPersonaEdicion(persona) {
+  personaEdicion.value = persona
+  if (persona !== null) {
+    titulo.value = 'Editar registros de Hoja: ' + persona.ID
+  } else {
+    titulo.value = 'Listado de hojas'
+  }
+}
 </script>
 
 <template>
-  <PersonasList></PersonasList>
+  <CargaFamiliarList
+    :personaEditar="persona"
+    :setPersonaEdicion="setPersonaEdicion"
+  ></CargaFamiliarList>
 </template>
