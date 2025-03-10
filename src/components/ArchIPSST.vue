5bd9efac57acd5a0f35214038e5458ef37dee987
José R. Ferreyra <jferreyra.dev@gmail.com>
2025-03-10 00:27:33 -0300

    Soporte descarga archivo IPSST

--- src/components/ArchIPSST.vue
+++ src/components/ArchIPSST.vue
@@ -1,4 +1,6 @@
-<script>
+<script setup>
+import { ref } from 'vue'
+import RepoHeader from './RepoHeader.vue'
 import { useFilterStore } from '@/stores/filterStore'
 import { useEndPoints } from '@/composables/useEndPoints'
 import { useFetch } from '@/composables/useFetch'
@@ -7,20 +9,47 @@ const { apiBase } = useEndPoints()
 
 const store = useFilterStore()
 
-function useResLiqCod(getId) {
-  return useFetch(
-    () =>
-      `${apiBase.value}/api/view/archivoIPSST?${getId()}&sort={"Cadena":"asc"}`
-  )
+function printData() {
+  let largeString = ""
+  for (let index = 0; index < data.value.length; index++) {
+    const element = data.value[index];
+    
+    largeString+= element.CADENA + '\n'
+  }
+
+  console.log(largeString )
+  downloadTxt(largeString,data.value[0].NOMBREARCHIVO)
 }
 
-const { data, error, isPending } = useResLiqCod(() => store.filterString)
+const downloadTxt = (contenido, nombre) => {
+    const a = document.createElement("a");
+    const archivo = new Blob([contenido], { type: 'text/plain' });
+    const url = URL.createObjectURL(archivo);
+    a.href = url;
+    a.download = nombre;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+function useLiqBoletas(getId) {
+  return useFetch(() => `${apiBase.value}/api/view/archivoIPSST?${getId()}`)
+}
+
+const { data, error, isPending } = useLiqBoletas(() => store.filterString)
+
+const headers = [
+  {
+    title: 'CADENA',
+    align: 'start',
+    key: 'CADENA'
+  }
+]
 </script>
 
 <template>
-  <v-container>
-    <RepoHeader title="Archivo IPSST" :subtitle="store.liqString">
-      <v-btn color="primary" @click="handleDownload" :disabled="!data">Descargar</v-btn>
+ <v-container>
+   <RepoHeader title="Archivo IPSST" :subtitle="store.liqString">
+      <v-btn color="primary" :disabled="!data" @click="printData" >Descargar</v-btn>
     </RepoHeader>
     <v-row>
       <div v-if="isPending">loading...</div>
@@ -32,8 +61,9 @@ const { data, error, isPending } = useResLiqCod(() => store.filterString)
         :items="data"
         :headers="headers"
       >
-      </v-data-table>
+      </v-data-table>      
       <div v-else-if="error">No se puede obtener los datos solicitados.</div>
+
     </v-row>
-  </v-container>
+  </v-container> 
 </template>
